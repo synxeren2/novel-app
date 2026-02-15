52098dfb042ce582a97f445ea502c893a7500d79
Yasin Eren Uyanık <yasineren277@gmail.com>
2026-02-15 15:20:33 +0300

    Update custom domain for email sender

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -6,7 +6,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/verify?token=${token}`;
 
   await resend.emails.send({
-    from: "noreply@yourdomain.com", // Bunu daha sonra kendi alan adınızla güncelleyebilirsiniz
+    from: "Roman Kütüphanesi <noreply@romanoku.space>", // Burayı kendi domaininle değiştir
     to: email,
     subject: "E-postanızı Doğrulayın",
     html: `<p>Roman uygulamasına hoş geldiniz! Hesabınızı doğrulamak için <a href="${confirmLink}">buraya tıklayın</a>.</p>`,
